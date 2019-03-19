49a8205aae29188e1111d41d3e2a0ed567d6fb8e
ElisDN <mail@elisdn.ru>
2019-03-19 20:25:18 +0300

    Added demo app

--- manager/public/index.php
+++ manager/public/index.php
@@ -2,4 +2,12 @@
 
 declare(strict_types=1);
 
-echo 'Hello, World!';
+echo '<html lang="en">
+<head>
+  <title>App</title>
+  <meta charset="UTF-8" />
+</head>
+<body>
+  <h1>Hello, World!</h1>
+</body>
+</html>';
