ac737795f9ca5b1ccbd8d988937c3b1bdd2c9346
ElisDN <mail@elisdn.ru>
2019-03-22 19:39:01 +0300

    Added Slim app

--- manager/public/index.php
+++ manager/public/index.php
@@ -2,12 +2,28 @@
 
 declare(strict_types=1);
 
-echo '<html lang="en">
-<head>
-  <title>App</title>
-  <meta charset="UTF-8" />
-</head>
-<body>
-  <h1>Hello, World!</h1>
-</body>
-</html>';
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+http_response_code(500);
+
+(function () {
+    $app = new \Slim\App([
+        'settings' => [
+            'addContentLengthHeader' => false,
+            'displayErrorDetails' => (bool)getenv('APP_DEBUG'),
+        ],
+    ]);
+
+    $app->get('/', function (Request $request, Response $response) {
+        return $response->withJson([
+            'name' => 'Manager',
+            'param' => $request->getQueryParam('param'),
+        ]);
+    });
+
+    $app->run();
+})();
