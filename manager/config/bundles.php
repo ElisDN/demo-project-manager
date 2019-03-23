ee8d0088a3497eb0851df6cb4174391134c257eb
ElisDN <mail@elisdn.ru>
2019-03-23 13:35:38 +0300

    Removed web server bundle

--- manager/config/bundles.php
+++ manager/config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
