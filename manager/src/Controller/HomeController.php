de3a84fce3129d95f24cd0aac02c9031071ebd19
ElisDN <mail@elisdn.ru>
2019-03-23 13:25:13 +0300

    Added home controller

---
+++ manager/src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('app/home.html.twig');
+    }
+}
