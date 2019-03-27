d31fb89461a034e91c01748e61f5d66ae0477eb6
ElisDN <mail@elisdn.ru>
2019-03-27 18:55:16 +0300

    Added login success handler

--- manager/src/Security/LoginFormAuthenticator.php
+++ manager/src/Security/LoginFormAuthenticator.php
@@ -85,8 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
