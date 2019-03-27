0bb7eaaeff1966b18e7ed3dfbd62e818937fabbd
ElisDN <mail@elisdn.ru>
2019-03-27 18:57:33 +0300

    Added access control

--- manager/src/Controller/SecurityController.php
+++ manager/src/Controller/SecurityController.php
@@ -21,4 +21,13 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
