5a1b2e47accf52493b85146b5ac93b04c66c1175
ElisDN <mail@elisdn.ru>
2019-03-27 17:09:02 +0300

    Added signup request drafts

---
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\SignUp\Request;
+
+class Handler
+{
+    public function handle(Command $command): void
+    {
+
+    }
+}
---
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+
+    }
+}
