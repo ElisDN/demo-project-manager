573ca94c22f42b3e656313f587eeda4c0c5f471b
ElisDN <mail@elisdn.ru>
2019-03-27 18:32:30 +0300

    Generated register form

--- manager/src/Entity/User.php
+++ manager/src/Entity/User.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
