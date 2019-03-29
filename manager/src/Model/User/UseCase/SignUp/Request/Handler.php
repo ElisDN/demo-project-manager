24b0f66e6b94de0dad017f60e47a36840f362fe3
ElisDN <mail@elisdn.ru>
2019-03-29 17:56:25 +0300

    Added signup request usecase

--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -4,10 +4,32 @@ declare(strict_types=1);
 
 namespace App\Model\User\UseCase\SignUp\Request;
 
+use App\Model\User\Entity\User\User;
+use Doctrine\ORM\EntityManagerInterface;
+
 class Handler
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function handle(Command $command): void
     {
+        $email = mb_strtolower($command->email);
 
+        if ($this->em->getRepository(User::class)->findOneBy(['email' => $email])) {
+            throw new \DomainException('User already exists.');
+        }
+
+        $user = new User(
+            $email,
+            password_hash($command->password, PASSWORD_ARGON2I)
+        );
+
+        $this->em->persist($user);
+        $this->em->flush();
     }
 }
--- manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
 
+use App\Model\User\Entity\User\User;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
     public function testSuccess(): void
     {
+        $user = new User(
+            $email = 'test@app.test',
+            $hash = 'hash'
+        );
 
+        self::assertEquals($email, $user->getEmail());
+        self::assertEquals($hash, $user->getPasswordHash());
     }
 }
