3d530bee62ed950562615ef5a59bef2191eb1e13
ElisDN <mail@elisdn.ru>
2019-03-29 19:32:20 +0300

    Added confirm token and wait status

--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -9,18 +9,30 @@ use App\Model\User\Entity\User\Email;
 use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\User;
 use App\Model\User\Entity\User\UserRepository;
+use App\Model\User\Service\ConfirmTokenizer;
+use App\Model\User\Service\ConfirmTokenSender;
 use App\Model\User\Service\PasswordHasher;
 
 class Handler
 {
     private $users;
     private $hasher;
+    private $tokenizer;
+    private $sender;
     private $flusher;
 
-    public function __construct(UserRepository $users, PasswordHasher $hasher, Flusher $flusher)
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        ConfirmTokenizer $tokenizer,
+        ConfirmTokenSender $sender,
+        Flusher $flusher
+    )
     {
         $this->users = $users;
         $this->hasher = $hasher;
+        $this->tokenizer = $tokenizer;
+        $this->sender = $sender;
         $this->flusher = $flusher;
     }
 
@@ -36,10 +48,13 @@ class Handler
             Id::next(),
             new \DateTimeImmutable(),
             $email,
-            $this->hasher->hash($command->password)
+            $this->hasher->hash($command->password),
+            $token = $this->tokenizer->generate()
         );
 
         $this->users->add($user);
+
+        $this->sender->send($email, $token);
 
         $this->flusher->flush();
     }
