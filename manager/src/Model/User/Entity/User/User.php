872e81e180b99964b7ef245ce8ad1162a786da1e
ElisDN <mail@elisdn.ru>
2019-03-29 18:09:47 +0300

    Added user create date

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -11,6 +11,10 @@ class User
      */
     private $id;
     /**
+     * @var \DateTimeImmutable
+     */
+    private $date;
+    /**
      * @var string
      */
     private $email;
@@ -19,9 +23,10 @@ class User
      */
     private $passwordHash;
 
-    public function __construct(string $id, string $email, string $hash)
+    public function __construct(string $id, \DateTimeImmutable $date, string $email, string $hash)
     {
         $this->id = $id;
+        $this->date = $date;
         $this->email = $email;
         $this->passwordHash = $hash;
     }
@@ -29,6 +34,11 @@ class User
     public function getId(): string
     {
         return $this->id;
+    }
+
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
     }
 
     public function getEmail(): string
