9eac5d766f8d9d5ef046d6b25305398e94d95f52
ElisDN <mail@elisdn.ru>
2019-03-29 17:57:37 +0300

    Added user draft

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -6,5 +6,28 @@ namespace App\Model\User\Entity\User;
 
 class User
 {
+    /**
+     * @var string
+     */
+    private $email;
+    /**
+     * @var string
+     */
+    private $passwordHash;
 
+    public function __construct(string $email, string $hash)
+    {
+        $this->email = $email;
+        $this->passwordHash = $hash;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
 }
