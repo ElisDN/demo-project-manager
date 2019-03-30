875a907bfebd5c6cc320379111245d92823957d4
ElisDN <mail@elisdn.ru>
2019-03-30 13:52:19 +0300

    Extracted user builder

--- manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -5,18 +5,14 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
 
 use App\Model\User\Entity\User\Email;
-use App\Model\User\Entity\User\Id;
-use App\Model\User\Entity\User\User;
+use App\Tests\Builder\User\UserBuilder;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $user = new User(
-            $id = Id::next(),
-            $date = new \DateTimeImmutable()
-        );
+        $user = (new UserBuilder())->build();
 
         $user->signUpByEmail(
             $email = new Email('test@app.test'),
@@ -34,10 +30,7 @@ class RequestTest extends TestCase
 
     public function testAlready(): void
     {
-        $user = new User(
-            $id = Id::next(),
-            $date = new \DateTimeImmutable()
-        );
+        $user = (new UserBuilder())->build();
 
         $user->signUpByEmail(
             $email = new Email('test@app.test'),
