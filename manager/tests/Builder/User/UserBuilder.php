e7db1ab8a58a1e2dd755e5ce8095d6efb40bbb13
ElisDN <mail@elisdn.ru>
2019-04-12 17:25:53 +0300

    Migrated to named constructors

--- manager/tests/Builder/User/UserBuilder.php
+++ manager/tests/Builder/User/UserBuilder.php
@@ -53,13 +53,10 @@ class UserBuilder
 
     public function build(): User
     {
-        $user = new User(
-            $this->id,
-            $this->date
-        );
-
         if ($this->email) {
-            $user->signUpByEmail(
+            $user = User::signUpByEmail(
+                $this->id,
+                $this->date,
                 $this->email,
                 $this->hash,
                 $this->token
@@ -68,15 +65,19 @@ class UserBuilder
             if ($this->confirmed) {
                 $user->confirmSignUp();
             }
+
+            return $user;
         }
 
         if ($this->network) {
-            $user->signUpByNetwork(
+            return User::signUpByNetwork(
+                $this->id,
+                $this->date,
                 $this->network,
                 $this->identity
             );
         }
 
-        return $user;
+        throw new \BadMethodCallException('Specify via method.');
     }
 }
--- manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
+++ manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
@@ -4,40 +4,30 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Model\User\Entity\User\Network;
 
+use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\Network;
-use App\Tests\Builder\User\UserBuilder;
+use App\Model\User\Entity\User\User;
 use PHPUnit\Framework\TestCase;
 
 class AuthTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $user = (new UserBuilder())->build();
-
-        $user->signUpByNetwork(
+        $user = User::signUpByNetwork(
+            $id = Id::next(),
+            $date = new \DateTimeImmutable(),
             $network = 'vk',
             $identity = '0000001'
         );
 
         self::assertTrue($user->isActive());
 
+        self::assertEquals($id, $user->getId());
+        self::assertEquals($date, $user->getDate());
+
         self::assertCount(1, $networks = $user->getNetworks());
         self::assertInstanceOf(Network::class, $first = reset($networks));
         self::assertEquals($network, $first->getNetwork());
         self::assertEquals($identity, $first->getIdentity());
-    }
-
-    public function testAlready(): void
-    {
-        $user = (new UserBuilder())->build();
-
-        $user->signUpByNetwork(
-            $network = 'vk',
-            $identity = '0000001'
-        );
-
-        $this->expectExceptionMessage('User is already signed up.');
-
-        $user->signUpByNetwork($network, $identity);
     }
 }
