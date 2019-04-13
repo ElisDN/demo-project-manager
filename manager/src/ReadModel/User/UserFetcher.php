b6050d5676216733a21081ee6f16708dd25bbb16
ElisDN <mail@elisdn.ru>
2019-04-13 17:29:59 +0300

    Added custom user provider

--- manager/src/ReadModel/User/UserFetcher.php
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\ReadModel\User;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
 
 class UserFetcher
 {
@@ -23,5 +24,25 @@ class UserFetcher
             ->where('reset_token_token = :token')
             ->setParameter(':token', $token)
             ->execute()->fetchColumn(0) > 0;
+    }
+
+    public function findForAuth(string $email): ?AuthView
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'email',
+                'password_hash',
+                'role'
+            )
+            ->from('user_users')
+            ->where('email = :email')
+            ->setParameter(':email', $email)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, AuthView::class);
+        $result = $stmt->fetch();
+
+        return $result ?: null;
     }
 }
