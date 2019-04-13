c8780735a8a98ca2bae6e10688a08672f8198c9c
ElisDN <mail@elisdn.ru>
2019-04-13 17:50:21 +0300

    Added email default sender plugin

--- manager/src/Model/User/Service/ResetTokenSender.php
+++ manager/src/Model/User/Service/ResetTokenSender.php
@@ -12,19 +12,16 @@ class ResetTokenSender
 {
     private $mailer;
     private $twig;
-    private $from;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
         $this->twig = $twig;
-        $this->from = $from;
     }
 
     public function send(Email $email, ResetToken $token): void
     {
         $message = (new \Swift_Message('Password resetting'))
-            ->setFrom($this->from)
             ->setTo($email->getValue())
             ->setBody($this->twig->render('mail/user/reset.html.twig', [
                 'token' => $token->getToken()
