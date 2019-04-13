4f3f704e1d5caa6d850915ed0ca0a15a93bedfc9
ElisDN <mail@elisdn.ru>
2019-04-13 17:50:21 +0300

    Added email default sender plugin

--- manager/src/Model/User/Service/ConfirmTokenSender.php
+++ manager/src/Model/User/Service/ConfirmTokenSender.php
@@ -11,19 +11,16 @@ class ConfirmTokenSender
 {
     private $mailer;
     private $twig;
-    private $from;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
         $this->twig = $twig;
-        $this->from = $from;
     }
 
     public function send(Email $email, string $token): void
     {
         $message = (new \Swift_Message('Sig Up Confirmation'))
-            ->setFrom($this->from)
             ->setTo($email->getValue())
             ->setBody($this->twig->render('mail/user/signup.html.twig', [
                 'token' => $token
