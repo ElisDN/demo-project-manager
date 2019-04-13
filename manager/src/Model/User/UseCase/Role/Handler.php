4e5a8ee8ac2786ec7868c50ef7bbde6b752ba914
ElisDN <mail@elisdn.ru>
2019-04-13 11:50:34 +0300

    Added role

---
+++ manager/src/Model/User/UseCase/Role/Handler.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Role;
+
+use App\Model\Flusher;
+use App\Model\User\Entity\User\Id;
+use App\Model\User\Entity\User\Role;
+use App\Model\User\Entity\User\UserRepository;
+
+class Handler
+{
+    private $users;
+    private $flusher;
+
+    public function __construct(UserRepository $users, Flusher $flusher)
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $user = $this->users->get(new Id($command->id));
+
+        $user->changeRole(new Role($command->role));
+
+        $this->flusher->flush();
+    }
+}
