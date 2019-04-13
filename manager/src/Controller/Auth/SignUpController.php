1c8061c12e43472a9a71c9dad96ba3b9ee78fd77
ElisDN <mail@elisdn.ru>
2019-04-13 13:28:13 +0300

    Added sign up page

---
+++ manager/src/Controller/Auth/SignUpController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Auth;
+
+use App\Model\User\UseCase\SignUp;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SignUpController extends AbstractController
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @Route("/signup", name="auth.signup")
+     * @param Request $request
+     * @param SignUp\Request\Handler $handler
+     * @return Response
+     */
+    public function request(Request $request, SignUp\Request\Handler $handler): Response
+    {
+        $command = new SignUp\Request\Command();
+
+        $form = $this->createForm(SignUp\Request\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                $this->addFlash('success', 'Check your email.');
+                return $this->redirectToRoute('home');
+            } catch (\DomainException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('app/auth/signup.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
