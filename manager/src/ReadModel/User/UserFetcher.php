d1fae6206018ddf98517707263fa34fea48272bb
ElisDN <mail@elisdn.ru>
2019-04-13 16:44:15 +0300

    Added read token checking

---
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\User;
+
+use Doctrine\DBAL\Connection;
+
+class UserFetcher
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function existsByResetToken(string $token): bool
+    {
+        return $this->connection->createQueryBuilder()
+            ->select('COUNT (*)')
+            ->from('user_users')
+            ->where('reset_token_token = :token')
+            ->setParameter(':token', $token)
+            ->execute()->fetchColumn(0) > 0;
+    }
+}
