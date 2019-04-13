55053f0c5823e6856e6e6f0cb32d5aba9678ef6b
ElisDN <mail@elisdn.ru>
2019-04-13 13:40:27 +0300

    Implemented confirm email sending

--- manager/src/Model/User/Service/ConfirmTokenSender.php
+++ manager/src/Model/User/Service/ConfirmTokenSender.php
@@ -5,11 +5,32 @@ declare(strict_types=1);
 namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
+use Twig\Environment;
 
 class ConfirmTokenSender
 {
+    private $mailer;
+    private $twig;
+    private $from;
+
+    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->from = $from;
+    }
+
     public function send(Email $email, string $token): void
     {
+        $message = (new \Swift_Message('Sig Up Confirmation'))
+            ->setFrom($this->from)
+            ->setTo($email->getValue())
+            ->setBody($this->twig->render('mail/user/signup.html.twig', [
+                'token' => $token
+            ]), 'text/html');
 
+        if (!$this->mailer->send($message)) {
+            throw new \RuntimeException('Unable to send message.');
+        }
     }
 }
