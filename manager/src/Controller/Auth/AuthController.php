54c1e8e2b6394f1bb313c31a804f0777a20051e0
ElisDN <mail@elisdn.ru>
2019-04-13 16:56:18 +0300

    Generated auth via email

---
+++ manager/src/Controller/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('app/auth/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+}
