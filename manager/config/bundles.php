f7e664a977c35e4b7d567e9a4fe1cc53cdc0c3bc
ElisDN <mail@elisdn.ru>
2019-04-20 15:47:58 +0300

    Installed encore

--- manager/config/bundles.php
+++ manager/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
