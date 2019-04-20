0e6be55e690db6a81013ecc15e8b25c20054557d
ElisDN <mail@elisdn.ru>
2019-04-20 12:46:41 +0300

    Added home test

---
+++ manager/tests/Functional/HomeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+    public function testGuest(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+        $this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
+    }
+
+    public function testSuccess(): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'admin@app.test',
+            'PHP_AUTH_PW' => 'password',
+        ]);
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Hello', $crawler->filter('h1')->text());
+    }
+}
