53c58c74e3d3eda09a14c28563fb80cf69b4c344
ElisDN <mail@elisdn.ru>
2019-04-26 17:08:18 +0300

    Moved profile controller

--- manager/src/Controller/Profile/ShowController.php
+++ manager/src/Controller/Profile/ShowController.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Controller\Profile;
 
 use App\ReadModel\User\UserFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfileController extends AbstractController
+class ShowController extends AbstractController
 {
     private $users;
 
@@ -22,7 +22,7 @@ class ProfileController extends AbstractController
      * @Route("/profile", name="profile")
      * @return Response
      */
-    public function index(): Response
+    public function show(): Response
     {
         $user = $this->users->findDetail($this->getUser()->getId());
 
