026f6453827845725537ded1eb17b677d1f9534b
ElisDN <mail@elisdn.ru>
2019-04-27 13:36:13 +0300

    Added user create page

---
+++ manager/src/Model/User/UseCase/Create/Handler.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Create;
+
+use App\Model\Flusher;
+use App\Model\User\Entity\User\Email;
+use App\Model\User\Entity\User\Name;
+use App\Model\User\Entity\User\User;
+use App\Model\User\Entity\User\Id;
+use App\Model\User\Entity\User\UserRepository;
+use App\Model\User\Service\PasswordGenerator;
+use App\Model\User\Service\PasswordHasher;
+
+class Handler
+{
+    private $users;
+    private $hasher;
+    private $generator;
+    private $flusher;
+
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        PasswordGenerator $generator,
+        Flusher $flusher
+    )
+    {
+        $this->users = $users;
+        $this->hasher = $hasher;
+        $this->generator = $generator;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $email = new Email($command->email);
+
+        if ($this->users->hasByEmail($email)) {
+            throw new \DomainException('User with this email already exists.');
+        }
+
+        $user = User::create(
+            Id::next(),
+            new \DateTimeImmutable(),
+            new Name(
+                $command->firstName,
+                $command->lastName
+            ),
+            $email,
+            $this->hasher->hash($this->generator->generate())
+        );
+
+        $this->users->add($user);
+
+        $this->flusher->flush();
+    }
+}
