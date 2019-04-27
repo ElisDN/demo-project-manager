c6e8e1d17018f88558637652b472817d28920328
ElisDN <mail@elisdn.ru>
2019-04-27 13:59:55 +0300

    Added user role page

--- manager/src/Model/User/UseCase/Role/Command.php
+++ manager/src/Model/User/UseCase/Role/Command.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model\User\UseCase\Role;
 
+use App\Model\User\Entity\User\User;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
@@ -22,5 +23,12 @@ class Command
     public function __construct(string $id)
     {
         $this->id = $id;
+    }
+
+    public static function fromUser(User $user): self
+    {
+        $command = new self($user->getId()->getValue());
+        $command->role = $user->getRole()->getName();
+        return $command;
     }
 }
---
+++ manager/src/Model/User/UseCase/Role/Form.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model\User\UseCase\Role;
+
+use App\Model\User\Entity\User\Role;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('role', Type\ChoiceType::class, ['label' => 'Role', 'choices' => [
+                'User' => Role::USER,
+                'Admin' => Role::ADMIN,
+            ]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Command::class,
+        ));
+    }
+}
