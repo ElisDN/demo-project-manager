a7150fb8315b6e08a102e1061520c26d2b937c47
ElisDN <mail@elisdn.ru>
2019-04-27 13:56:07 +0300

    Added user edit page

---
+++ manager/src/Model/User/UseCase/Edit/Command.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Edit;
+
+use App\Model\User\Entity\User\User;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    public $email;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $firstName;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $lastName;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function fromUser(User $user): self
+    {
+        $command = new self($user->getId()->getValue());
+        $command->email = $user->getEmail() ? $user->getEmail()->getValue() : null;
+        $command->firstName = $user->getName()->getFirst();
+        $command->lastName = $user->getName()->getLast();
+        return $command;
+    }
+}
