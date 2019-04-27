fa4eaafc8c2f905d353f995926081ae61fc473a5
ElisDN <mail@elisdn.ru>
2019-04-27 17:16:11 +0300

    Added identity display name

--- manager/src/Security/UserProvider.php
+++ manager/src/Security/UserProvider.php
@@ -62,6 +62,7 @@ class UserProvider implements UserProviderInterface
             $user->id,
             $user->email ?: $username,
             $user->password_hash ?: '',
+            $user->name ?: $username,
             $user->role,
             $user->status
         );
