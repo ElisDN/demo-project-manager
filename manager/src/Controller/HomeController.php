e5f3fc2b9aa7e906843ebf7c9eccb05b1a5d51c9
ElisDN <mail@elisdn.ru>
2019-05-03 21:39:26 +0300

    Added credits checker

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Annotation\RequiresUserCredits;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @RequiresUserCredits()
+ */
 class HomeController extends AbstractController
 {
     /**
