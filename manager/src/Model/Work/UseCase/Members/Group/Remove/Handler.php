32f03fbd8ca2eada567edbd820bcede2e5a4792e
ElisDN <mail@elisdn.ru>
2019-05-03 19:09:57 +0300

    Added group removing protection

--- manager/src/Model/Work/UseCase/Members/Group/Remove/Handler.php
+++ manager/src/Model/Work/UseCase/Members/Group/Remove/Handler.php
@@ -7,21 +7,29 @@ namespace App\Model\Work\UseCase\Members\Group\Remove;
 use App\Model\Flusher;
 use App\Model\Work\Entity\Members\Group\GroupRepository;
 use App\Model\Work\Entity\Members\Group\Id;
+use App\Model\Work\Entity\Members\Member\MemberRepository;
 
 class Handler
 {
     private $groups;
+    private $members;
     private $flusher;
 
-    public function __construct(GroupRepository $groups, Flusher $flusher)
+    public function __construct(GroupRepository $groups, MemberRepository $members, Flusher $flusher)
     {
+
         $this->groups = $groups;
+        $this->members = $members;
         $this->flusher = $flusher;
     }
 
     public function handle(Command $command): void
     {
         $group = $this->groups->get(new Id($command->id));
+
+        if ($this->members->hasByGroup($group->getId())) {
+            throw new \DomainException('Group is not empty.');
+        }
 
         $this->groups->remove($group);
 
