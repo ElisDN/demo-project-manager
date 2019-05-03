afcd0011c65cf89f9d83c9a04a57ec17eb02f3d6
ElisDN <mail@elisdn.ru>
2019-05-03 18:02:32 +0300

    Moved access control to annotations

--- manager/src/Controller/UsersController.php
+++ manager/src/Controller/UsersController.php
@@ -14,6 +14,7 @@ use App\Model\User\UseCase\SignUp\Confirm;
 use App\ReadModel\User\Filter;
 use App\ReadModel\User\UserFetcher;
 use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,6 +22,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/users", name="users")
+ * @IsGranted("ROLE_MANAGE_USERS")
  */
 class UsersController extends AbstractController
 {
