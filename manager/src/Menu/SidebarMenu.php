76147f5440752b0dd4dc79c0f766c04be452f035
ElisDN <mail@elisdn.ru>
2019-05-03 21:38:33 +0300

    Added KNP Menu

---
+++ manager/src/Menu/SidebarMenu.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SidebarMenu
+{
+    private $factory;
+    private $auth;
+
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $auth)
+    {
+        $this->factory = $factory;
+        $this->auth = $auth;
+    }
+
+    public function build(): ItemInterface
+    {
+        $menu = $this->factory->createItem('root')
+            ->setChildrenAttributes(['class' => 'nav']);
+
+        $menu->addChild('Dashboard', ['route' => 'home'])
+            ->setExtra('icon', 'nav-icon icon-speedometer')
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link');
+
+        $menu->addChild('Work')->setAttribute('class', 'nav-title');
+
+        if ($this->auth->isGranted('ROLE_WORK_MANAGE_MEMBERS')) {
+            $menu->addChild('Members', ['route' => 'work.members'])
+                ->setExtra('routes', [
+                    ['route' => 'work.members'],
+                    ['pattern' => '/^work\.members\..+/']
+                ])
+                ->setExtra('icon', 'nav-icon icon-people')
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
+        }
+
+        $menu->addChild('Control')->setAttribute('class', 'nav-title');
+
+        if ($this->auth->isGranted('ROLE_MANAGE_USERS')) {
+            $menu->addChild('Users', ['route' => 'users'])
+                ->setExtra('icon', 'nav-icon icon-people')
+                ->setExtra('routes', [
+                    ['route' => 'users'],
+                    ['pattern' => '/^users\..+/']
+                ])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
+        }
+
+        $menu->addChild('Profile', ['route' => 'profile'])
+            ->setExtra('icon', 'nav-icon icon-user')
+            ->setExtra('routes', [
+                ['route' => 'profile'],
+                ['pattern' => '/^profile\..+/']
+            ])
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link');
+
+        return $menu;
+    }
+}
