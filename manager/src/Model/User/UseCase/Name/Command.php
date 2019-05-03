cbee935b755e93dc37297b066b7c893a7b6a0f97
ElisDN <mail@elisdn.ru>
2019-05-03 14:26:41 +0300

    Changed user fetcher

--- manager/src/Model/User/UseCase/Name/Command.php
+++ manager/src/Model/User/UseCase/Name/Command.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model\User\UseCase\Name;
 
+use App\Model\User\Entity\User\User;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
@@ -27,5 +28,13 @@ class Command
     public function __construct(string $id)
     {
         $this->id = $id;
+    }
+
+    public static function fromUser(User $user): self
+    {
+        $command = new self($user->getId()->getValue());
+        $command->firstName = $user->getName()->getFirst();
+        $command->lastName = $user->getName()->getLast();
+        return $command;
     }
 }
