bc0818fe79237cb42b786b1cb610dab6875e830d
ElisDN <mail@elisdn.ru>
2019-05-03 21:39:26 +0300

    Added credits checker

---
+++ manager/src/Annotation/RequiresUserCredits.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * @Annotation
+ * @Target({"CLASS", "METHOD"})
+ */
+class RequiresUserCredits
+{
+
+}
