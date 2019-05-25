54b85ff14cd08606cb4279d49a7f7133a5dba13f
ElisDN <mail@elisdn.ru>
2019-05-25 16:46:43 +0300

    Added project members fixture

--- manager/src/DataFixtures/Work/Projects/ProjectFixture.php
+++ manager/src/DataFixtures/Work/Projects/ProjectFixture.php
@@ -4,19 +4,30 @@ declare(strict_types=1);
 
 namespace App\DataFixtures\Work\Projects;
 
+use App\DataFixtures\Work\Members\MemberFixture;
+use App\Model\User\Entity\User\Role;
+use App\Model\Work\Entity\Members\Member\Member;
 use App\Model\Work\Entity\Projects\Project\Department\Id as DepartmentId;
 use App\Model\Work\Entity\Projects\Project\Project;
 use App\Model\Work\Entity\Projects\Project\Id;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ProjectFixture extends Fixture
+class ProjectFixture extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        /** @var Member $admin */
+        $admin = $this->getReference(MemberFixture::REFERENCE_ADMIN);
+
+        /** @var Role $manage */
+        $manage = $this->getReference(RoleFixture::REFERENCE_MANAGER);
+
         $active = $this->createProject('First Project', 1);
-        $active->addDepartment(DepartmentId::next(), 'Development');
+        $active->addDepartment($development = DepartmentId::next(), 'Development');
         $active->addDepartment(DepartmentId::next(), 'Marketing');
+        $active->addMember($admin, [$development], [$manage]);
         $manager->persist($active);
 
         $active = $this->createProject('Second Project', 2);
@@ -42,5 +53,13 @@ class ProjectFixture extends Fixture
             $name,
             $sort
         );
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            MemberFixture::class,
+            RoleFixture::class,
+        ];
     }
 }
