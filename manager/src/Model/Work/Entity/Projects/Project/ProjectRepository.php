a46abbac39c3d4067d7f8a01a28362c3dec419eb
ElisDN <mail@elisdn.ru>
2019-05-25 16:06:32 +0300

    Added role remove restriction

--- manager/src/Model/Work/Entity/Projects/Project/ProjectRepository.php
+++ manager/src/Model/Work/Entity/Projects/Project/ProjectRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Model\Work\Entity\Projects\Project;
 
 use App\Model\EntityNotFoundException;
+use App\Model\Work\Entity\Projects\Role\Id as RoleId;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ProjectRepository
@@ -19,6 +20,17 @@ class ProjectRepository
     {
         $this->repo = $em->getRepository(Project::class);
         $this->em = $em;
+    }
+
+    public function hasMembersWithRole(RoleId $id): bool
+    {
+        return $this->repo->createQueryBuilder('p')
+                ->select('COUNT(p.id)')
+                ->innerJoin('p.memberships', 'ms')
+                ->innerJoin('ms.roles', 'r')
+                ->andWhere('r.id = :role')
+                ->setParameter(':role', $id->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
     }
 
     public function get(Id $id): Project
