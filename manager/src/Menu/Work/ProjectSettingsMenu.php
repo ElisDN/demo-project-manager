d3d1f9662b68c86c036ae897c207bb23cec314e8
ElisDN <mail@elisdn.ru>
2019-05-25 14:55:14 +0300

    Added project members controller

--- manager/src/Menu/Work/ProjectSettingsMenu.php
+++ manager/src/Menu/Work/ProjectSettingsMenu.php
@@ -48,6 +48,18 @@ class ProjectSettingsMenu
                 ])
                 ->setAttribute('class', 'nav-item')
                 ->setLinkAttribute('class', 'nav-link');
+
+            $menu
+                ->addChild('Members', [
+                    'route' => 'work.projects.project.settings.members',
+                    'routeParameters' => ['project_id' => $options['project_id']]
+                ])
+                ->setExtra('routes', [
+                    ['route' => 'work.projects.project.settings.members'],
+                    ['pattern' => '/^work.projects.project.settings.members\..+/']
+                ])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
         }
 
         return $menu;
