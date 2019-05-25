2ae20b383ab655fc38bb9a3628a026eb22e179ad
ElisDN <mail@elisdn.ru>
2019-05-25 16:07:56 +0300

    Added departments members count

--- manager/src/Controller/Work/Projects/Project/Settings/DepartmentsController.php
+++ manager/src/Controller/Work/Projects/Project/Settings/DepartmentsController.php
@@ -10,6 +10,7 @@ use App\Model\Work\Entity\Projects\Project\Project;
 use App\Model\Work\UseCase\Projects\Project\Department\Create;
 use App\Model\Work\UseCase\Projects\Project\Department\Edit;
 use App\Model\Work\UseCase\Projects\Project\Department\Remove;
+use App\ReadModel\Work\Projects\Project\DepartmentFetcher;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -35,13 +36,14 @@ class DepartmentsController extends AbstractController
     /**
      * @Route("", name="")
      * @param Project $project
+     * @param DepartmentFetcher $departments
      * @return Response
      */
-    public function index(Project $project): Response
+    public function index(Project $project, DepartmentFetcher $departments): Response
     {
         return $this->render('app/work/projects/project/settings/departments/index.html.twig', [
             'project' => $project,
-            'departments' => $project->getDepartments(),
+            'departments' => $departments->allOfProject($project->getId()->getValue()),
         ]);
     }
 
--- manager/src/ReadModel/Work/Projects/Project/DepartmentFetcher.php
+++ manager/src/ReadModel/Work/Projects/Project/DepartmentFetcher.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\ReadModel\Work\Projects\Project;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
 
 class DepartmentFetcher
 {
@@ -29,5 +30,27 @@ class DepartmentFetcher
             ->execute();
 
         return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
+    }
+
+    public function allOfProject(string $project): array
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'd.id',
+                'd.name',
+                '(
+                    SELECT COUNT(ms.member_id)
+                    FROM work_projects_project_memberships ms
+                    INNER JOIN work_projects_project_membership_departments md ON ms.id = md.membership_id
+                    WHERE md.department_id = d.id AND ms.project_id = :project
+                ) AS members_count'
+            )
+            ->from('work_projects_project_departments', 'd')
+            ->andWhere('project_id = :project')
+            ->setParameter(':project', $project)
+            ->orderBy('name')
+            ->execute();
+
+        return $stmt->fetchAll(FetchMode::ASSOCIATIVE);
     }
 }
