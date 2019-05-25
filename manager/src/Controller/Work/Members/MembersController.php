df678d0bd814c105c78d968156c8e9ecf4eeb4cc
ElisDN <mail@elisdn.ru>
2019-05-25 22:57:12 +0300

    Added member membership display

--- manager/src/Controller/Work/Members/MembersController.php
+++ manager/src/Controller/Work/Members/MembersController.php
@@ -14,6 +14,7 @@ use App\Model\Work\UseCase\Members\Member\Create;
 use App\Model\Work\UseCase\Members\Member\Move;
 use App\ReadModel\Work\Members\Member\Filter;
 use App\ReadModel\Work\Members\Member\MemberFetcher;
+use App\ReadModel\Work\Projects\Project\DepartmentFetcher;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -219,10 +220,13 @@ class MembersController extends AbstractController
     /**
      * @Route("/{id}", name=".show", requirements={"id"=Guid::PATTERN})
      * @param Member $member
+     * @param DepartmentFetcher $fetcher
      * @return Response
      */
-    public function show(Member $member): Response
+    public function show(Member $member, DepartmentFetcher $fetcher): Response
     {
-        return $this->render('app/work/members/show.html.twig', compact('member'));
+        $departments = $fetcher->allOfMember($member->getId()->getValue());
+
+        return $this->render('app/work/members/show.html.twig', compact('member', 'departments'));
     }
 }
--- manager/src/ReadModel/Work/Projects/Project/DepartmentFetcher.php
+++ manager/src/ReadModel/Work/Projects/Project/DepartmentFetcher.php
@@ -53,4 +53,25 @@ class DepartmentFetcher
 
         return $stmt->fetchAll(FetchMode::ASSOCIATIVE);
     }
+
+    public function allOfMember(string $member): array
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'p.id AS project_id',
+                'p.name AS project_name',
+                'd.id AS department_id',
+                'd.name AS department_name'
+            )
+            ->from('work_projects_project_memberships', 'ms')
+            ->innerJoin('ms', 'work_projects_project_membership_departments', 'msd', 'ms.id = msd.membership_id')
+            ->innerJoin('msd', 'work_projects_project_departments', 'd', 'msd.department_id = d.id')
+            ->innerJoin('d', 'work_projects_projects', 'p', 'd.project_id = p.id')
+            ->andWhere('ms.member_id = :member')
+            ->setParameter(':member', $member)
+            ->orderBy('p.sort')->addOrderBy('d.name')
+            ->execute();
+
+        return $stmt->fetchAll(FetchMode::ASSOCIATIVE);
+    }
 }
