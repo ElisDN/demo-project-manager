88a21261b805d97f82709183665409f083a88973
ElisDN <mail@elisdn.ru>
2019-05-25 16:16:19 +0300

    Added projects for members listing

--- manager/src/ReadModel/Work/Projects/Project/Filter/Filter.php
+++ manager/src/ReadModel/Work/Projects/Project/Filter/Filter.php
@@ -8,6 +8,22 @@ use App\Model\Work\Entity\Members\Member\Status;
 
 class Filter
 {
+    public $member;
     public $name;
     public $status = Status::ACTIVE;
+
+    private function __construct(?string $member)
+    {
+        $this->member = $member;
+    }
+
+    public static function all(): self
+    {
+        return new self(null);
+    }
+
+    public static function forMember(string $id): self
+    {
+        return new self($id);
+    }
 }
