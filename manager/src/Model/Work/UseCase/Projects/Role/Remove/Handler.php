d83960cff89d6423d57c733021cfd5a9ff727d3b
ElisDN <mail@elisdn.ru>
2019-05-25 16:06:32 +0300

    Added role remove restriction

--- manager/src/Model/Work/UseCase/Projects/Role/Remove/Handler.php
+++ manager/src/Model/Work/UseCase/Projects/Role/Remove/Handler.php
@@ -5,24 +5,31 @@ declare(strict_types=1);
 namespace App\Model\Work\UseCase\Projects\Role\Remove;
 
 use App\Model\Flusher;
+use App\Model\Work\Entity\Projects\Project\ProjectRepository;
 use App\Model\Work\Entity\Projects\Role\RoleRepository;
 use App\Model\Work\Entity\Projects\Role\Id;
 
 class Handler
 {
     private $roles;
+    private $projects;
     private $flusher;
 
-    public function __construct(RoleRepository $roles, Flusher $flusher)
+    public function __construct(RoleRepository $roles, ProjectRepository $projects, Flusher $flusher)
     {
 
         $this->roles = $roles;
+        $this->projects = $projects;
         $this->flusher = $flusher;
     }
 
     public function handle(Command $command): void
     {
         $role = $this->roles->get(new Id($command->id));
+
+        if ($this->projects->hasMembersWithRole($role->getId())) {
+            throw new \DomainException('Role contains members.');
+        }
 
         $this->roles->remove($role);
 
