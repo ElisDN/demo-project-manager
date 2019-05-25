77f09d59e9f80a795dd1c5032f5b9166419a6718
ElisDN <mail@elisdn.ru>
2019-05-25 15:55:48 +0300

    Added roles controller

---
+++ manager/src/ReadModel/Work/Projects/RoleFetcher.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\Work\Projects;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\FetchMode;
+
+class RoleFetcher
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function all(): array
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'r.id',
+                'r.name',
+                'r.permissions'
+            )
+            ->from('work_projects_roles', 'r')
+            ->orderBy('name')
+            ->execute();
+
+        return array_map(static function (array $role) {
+            return array_replace($role, [
+                'permissions' => json_decode($role['permissions'], true)
+            ]);
+        }, $stmt->fetchAll(FetchMode::ASSOCIATIVE));
+    }
+}
+
