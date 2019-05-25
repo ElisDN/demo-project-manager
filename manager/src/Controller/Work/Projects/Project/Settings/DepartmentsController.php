716d7316367be7d792c29e4ce75277663cb7f051
ElisDN <mail@elisdn.ru>
2019-05-25 15:05:36 +0300

    Migrated to members access

--- manager/src/Controller/Work/Projects/Project/Settings/DepartmentsController.php
+++ manager/src/Controller/Work/Projects/Project/Settings/DepartmentsController.php
@@ -11,8 +11,8 @@ use App\Model\Work\UseCase\Projects\Project\Department\Create;
 use App\Model\Work\UseCase\Projects\Project\Department\Edit;
 use App\Model\Work\UseCase\Projects\Project\Department\Remove;
 use App\ReadModel\Work\Projects\Project\DepartmentFetcher;
+use App\Security\Voter\Work\ProjectAccess;
 use Psr\Log\LoggerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,7 +22,6 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/work/projects/{project_id}/settings/departments", name="work.projects.project.settings.departments")
  * @ParamConverter("project", options={"id" = "project_id"})
- * @IsGranted("ROLE_WORK_MANAGE_PROJECTS")
  */
 class DepartmentsController extends AbstractController
 {
@@ -41,6 +40,8 @@ class DepartmentsController extends AbstractController
      */
     public function index(Project $project, DepartmentFetcher $departments): Response
     {
+        $this->denyAccessUnlessGranted(ProjectAccess::MANAGE_MEMBERS, $project);
+
         return $this->render('app/work/projects/project/settings/departments/index.html.twig', [
             'project' => $project,
             'departments' => $departments->allOfProject($project->getId()->getValue()),
@@ -56,6 +57,8 @@ class DepartmentsController extends AbstractController
      */
     public function create(Project $project, Request $request, Create\Handler $handler): Response
     {
+        $this->denyAccessUnlessGranted(ProjectAccess::MANAGE_MEMBERS, $project);
+
         $command = new Create\Command($project->getId()->getValue());
 
         $form = $this->createForm(Create\Form::class, $command);
@@ -87,6 +90,8 @@ class DepartmentsController extends AbstractController
      */
     public function edit(Project $project, string $id, Request $request, Edit\Handler $handler): Response
     {
+        $this->denyAccessUnlessGranted(ProjectAccess::MANAGE_MEMBERS, $project);
+
         $department = $project->getDepartment(new Id($id));
 
         $command = Edit\Command::fromDepartment($project, $department);
@@ -121,6 +126,8 @@ class DepartmentsController extends AbstractController
      */
     public function delete(Project $project, string $id, Request $request, Remove\Handler $handler): Response
     {
+        $this->denyAccessUnlessGranted(ProjectAccess::MANAGE_MEMBERS, $project);
+
         if (!$this->isCsrfTokenValid('delete', $request->request->get('token'))) {
             return $this->redirectToRoute('work.projects.project.settings.departments', ['project_id' => $project->getId()]);
         }
--- manager/src/Model/Work/Entity/Projects/Project/Project.php
+++ manager/src/Model/Work/Entity/Projects/Project/Project.php
@@ -123,6 +123,16 @@ class Project
         throw new \DomainException('Department is not found.');
     }
 
+    public function hasMember(MemberId $id): bool
+    {
+        foreach ($this->memberships as $membership) {
+            if ($membership->isForMember($id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * @param Member $member
      * @param DepartmentId[] $departmentIds
@@ -166,6 +176,16 @@ class Project
             }
         }
         throw new \DomainException('Member is not found.');
+    }
+
+    public function isMemberGranted(MemberId $id, string $permission): bool
+    {
+        foreach ($this->memberships as $membership) {
+            if ($membership->isForMember($id)) {
+                return $membership->isGranted($permission);
+            }
+        }
+        return false;
     }
 
     public function isArchived(): bool
