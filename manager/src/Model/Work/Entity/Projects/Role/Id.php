ddc78a7b3148f54b4984a5b8d0697d98903c6388
ElisDN <mail@elisdn.ru>
2019-05-25 15:50:26 +0300

    Added projects role model

---
+++ manager/src/Model/Work/Entity/Projects/Role/Id.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\Entity\Projects\Role;
+
+use Ramsey\Uuid\Uuid;
+use Webmozart\Assert\Assert;
+
+class Id
+{
+    private $value;
+
+    public function __construct(string $value)
+    {
+        Assert::notEmpty($value);
+        $this->value = $value;
+    }
+
+    public static function next(): self
+    {
+        return new self(Uuid::uuid4()->toString());
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
