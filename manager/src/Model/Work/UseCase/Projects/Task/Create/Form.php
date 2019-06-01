fe8d73f8a4a181d2b5db413879f7e06473463f3a
ElisDN <mail@elisdn.ru>
2019-06-01 18:19:31 +0300

    Added multiple tasks adding

--- manager/src/Model/Work/UseCase/Projects/Task/Create/Form.php
+++ manager/src/Model/Work/UseCase/Projects/Task/Create/Form.php
@@ -15,7 +15,7 @@ class Form extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', Type\TextType::class)
+            ->add('names', NamesType::class, ['attr' => ['rows' => 3]])
             ->add('content', Type\TextareaType::class, ['required' => false, 'attr' => ['rows' => 10]])
             ->add('parent', Type\IntegerType::class, ['required' => false])
             ->add('plan', Type\DateType::class, ['required' => false, 'widget' => 'single_text', 'input' => 'datetime_immutable'])
