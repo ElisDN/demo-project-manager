e10d65c504dd84724470fd2237f2acd11e47e495
ElisDN <mail@elisdn.ru>
2019-06-01 16:13:20 +0300

    Added task executors

--- manager/src/Model/Work/Entity/Projects/Task/Task.php
+++ manager/src/Model/Work/Entity/Projects/Task/Task.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Model\Work\Entity\Projects\Task;
 
+use App\Model\Work\Entity\Members\Member\Id as MemberId;
 use App\Model\Work\Entity\Members\Member\Member;
 use App\Model\Work\Entity\Projects\Project\Project;
+use Doctrine\Common\Collections\ArrayCollection;
 use Webmozart\Assert\Assert;
 
 class Task
@@ -22,6 +24,7 @@ class Task
     private $priority;
     private $parent;
     private $status;
+    private $executors;
 
     public function __construct(
         Id $id,
@@ -44,6 +47,7 @@ class Task
         $this->type = $type;
         $this->priority = $priority;
         $this->status = Status::new();
+        $this->executors = new ArrayCollection();
     }
 
     public function edit(string $name, ?string $content): void
@@ -114,6 +118,35 @@ class Task
         $this->priority = $priority;
     }
 
+    public function hasExecutor(MemberId $id): bool
+    {
+        foreach ($this->executors as $executor) {
+            if ($executor->getId()->isEqual($id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function assignExecutor(Member $executor): void
+    {
+        if ($this->executors->contains($executor)) {
+            throw new \DomainException('Executor is already assigned.');
+        }
+        $this->executors->add($executor);
+    }
+
+    public function revokeExecutor(MemberId $id): void
+    {
+        foreach ($this->executors as $current) {
+            if ($current->getId()->isEqual($id)) {
+                $this->executors->removeElement($current);
+                return;
+            }
+        }
+        throw new \DomainException('Executor is not assigned.');
+    }
+
     public function isNew(): bool
     {
         return $this->status->isNew();
@@ -178,5 +211,13 @@ class Task
     public function getStatus(): Status
     {
         return $this->status;
+    }
+
+    /**
+     * @return Member[]
+     */
+    public function getExecutors(): array
+    {
+        return $this->executors->toArray();
     }
 }
