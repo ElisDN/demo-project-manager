4f60c49b3e270585306dc0b48c6c246893c94d0a
ElisDN <mail@elisdn.ru>
2019-06-01 16:45:53 +0300

    Added tasc create usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Create/Command.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\Create;
+
+use App\Model\Work\Entity\Projects\Task\Type;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $project;
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $member;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $content;
+    /**
+     * @var int
+     */
+    public $parent;
+    /**
+     * @var \DateTimeImmutable
+     * @Assert\Date()
+     */
+    public $plan;
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $type;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $priority;
+
+    public function __construct(string $project, string $member)
+    {
+        $this->project = $project;
+        $this->member = $member;
+        $this->type = Type::NONE;
+        $this->priority = 2;
+    }
+}
