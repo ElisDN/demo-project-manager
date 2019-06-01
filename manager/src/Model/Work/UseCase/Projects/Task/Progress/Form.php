6da24abdd6fa5e76c6af9dc400441a04bb61e56e
ElisDN <mail@elisdn.ru>
2019-06-01 17:03:56 +0300

    Added task simple usecases

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Progress/Form.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\Progress;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('progress', Type\ChoiceType::class, ['choices' => [
+                0 => 0,
+                25 => 25,
+                50 => 50,
+                75 => 75,
+                100 => 100
+            ], 'attr' => ['onchange' => 'this.form.submit()']]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Command::class,
+        ));
+    }
+}
