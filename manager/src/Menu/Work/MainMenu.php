cb4c0cd51fa6707727ea44b1ae8490c3669e79a3
ElisDN <mail@elisdn.ru>
2019-06-01 18:01:29 +0300

    Added tasks controllers

--- manager/src/Menu/Work/MainMenu.php
+++ manager/src/Menu/Work/MainMenu.php
@@ -29,6 +29,15 @@ class MainMenu
             ->setAttribute('class', 'nav-item')
             ->setLinkAttribute('class', 'nav-link');
 
+        $menu
+            ->addChild('Tasks', ['route' => 'work.projects.tasks'])
+            ->setExtra('routes', [
+                ['route' => 'work.projects.tasks'],
+                ['pattern' => '/^work.projects.tasks\..+/']
+            ])
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link');
+
         if ($this->auth->isGranted('ROLE_WORK_MANAGE_PROJECTS')) {
             $menu
                 ->addChild('Roles', ['route' => 'work.projects.roles'])
