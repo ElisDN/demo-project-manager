97afa50b7b7819eed08fc63333b6cba9c39ea036
ElisDN <mail@elisdn.ru>
2019-06-01 18:02:21 +0300

    Added tasks presets menu

--- manager/src/ReadModel/Work/Projects/Task/Filter/Filter.php
+++ manager/src/ReadModel/Work/Projects/Task/Filter/Filter.php
@@ -36,4 +36,18 @@ class Filter
         $clone->member = $member;
         return $clone;
     }
+
+    public function forAuthor(string $author): self
+    {
+        $clone = clone $this;
+        $clone->author = $author;
+        return $clone;
+    }
+
+    public function forExecutor(string $executor): self
+    {
+        $clone = clone $this;
+        $clone->executor = $executor;
+        return $clone;
+    }
 }
