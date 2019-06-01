e4b3c87eb0c5a838394e2ae9243747f44f4ad31a
ElisDN <mail@elisdn.ru>
2019-06-01 17:54:33 +0300

    Disabled sequence autocalling

--- manager/src/Model/Work/Entity/Projects/Task/Task.php
+++ manager/src/Model/Work/Entity/Projects/Task/Task.php
@@ -22,7 +22,7 @@ class Task
     /**
      * @var Id
      * @ORM\Column(type="work_projects_task_id")
-     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\GeneratedValue(strategy="NONE")
      * @ORM\SequenceGenerator(sequenceName="work_projects_tasks_seq", initialValue=1)
      * @ORM\Id
      */
