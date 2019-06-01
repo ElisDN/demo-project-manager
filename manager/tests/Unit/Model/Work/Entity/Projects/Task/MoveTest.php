cb4c4d40bbeb7a2114c1b712ce9a5ab25a6a4a8c
ElisDN <mail@elisdn.ru>
2019-06-01 15:50:33 +0300

    Added task moving

---
+++ manager/tests/Unit/Model/Work/Entity/Projects/Task/MoveTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\Work\Entity\Projects\Task;
+
+use App\Tests\Builder\Work\Members\GroupBuilder;
+use App\Tests\Builder\Work\Members\MemberBuilder;
+use App\Tests\Builder\Work\Projects\ProjectBuilder;
+use App\Tests\Builder\Work\Projects\TaskBuilder;
+use PHPUnit\Framework\TestCase;
+
+class MoveTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $destination = (new ProjectBuilder())->build();
+
+        $task->move($destination);
+
+        self::assertEquals($destination, $task->getProject());
+    }
+
+    public function testAlready(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $this->expectExceptionMessage('Project is already same.');
+        $task->move($project);
+    }
+}
