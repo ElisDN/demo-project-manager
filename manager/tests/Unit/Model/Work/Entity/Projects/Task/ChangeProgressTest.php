bbfef2150afbe00bdb5e9867858b0461037efc31
ElisDN <mail@elisdn.ru>
2019-06-01 16:04:20 +0300

    Added task progress changing

---
+++ manager/tests/Unit/Model/Work/Entity/Projects/Task/ChangeProgressTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\Work\Entity\Projects\Task;
+
+use App\Tests\Builder\Work\Members\GroupBuilder;
+use App\Tests\Builder\Work\Members\MemberBuilder;
+use App\Tests\Builder\Work\Projects\ProjectBuilder;
+use App\Tests\Builder\Work\Projects\TaskBuilder;
+use PHPUnit\Framework\TestCase;
+
+class ChangeProgressTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $task->changeProgress($progress = 25);
+
+        self::assertEquals($progress, $task->getProgress());
+    }
+
+    public function testAlready(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $task->changeProgress($progress = 25);
+
+        $this->expectExceptionMessage('Progress is already same.');
+        $task->changeProgress($progress);
+    }
+
+    public function testIncorrect(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $task->changeProgress(200);
+    }
+}
