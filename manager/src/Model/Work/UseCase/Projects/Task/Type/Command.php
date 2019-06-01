d6af61201059524a3cd1432d040c568f0147fb26
ElisDN <mail@elisdn.ru>
2019-06-01 17:03:56 +0300

    Added task simple usecases

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Type/Command.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\Type;
+
+use App\Model\Work\Entity\Projects\Task\Task;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $type;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function fromTask(Task $task): self
+    {
+        $command = new self($task->getId()->getValue());
+        $command->type = $task->getType()->getName();
+        return $command;
+    }
+}
+
