6a39c2171cb31e6887cccba378dab4a5788b941f
ElisDN <mail@elisdn.ru>
2019-06-01 17:33:29 +0300

    Added task take usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Take/Handler.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\Take;
+
+use App\Model\Flusher;
+use App\Model\Work\Entity\Projects\Task\Id;
+use App\Model\Work\Entity\Projects\Task\TaskRepository;
+use App\Model\Work\Entity\Members\Member\MemberRepository;
+use App\Model\Work\Entity\Members\Member\Id as MemberId;
+
+class Handler
+{
+    private $tasks;
+    private $flusher;
+    private $members;
+
+    public function __construct(
+        TaskRepository $tasks,
+        MemberRepository $members,
+        Flusher $flusher
+    )
+    {
+        $this->tasks = $tasks;
+        $this->flusher = $flusher;
+        $this->members = $members;
+    }
+
+    public function handle(Command $command): void
+    {
+        $task = $this->tasks->get(new Id($command->id));
+        $member = $this->members->get(new MemberId($command->member));
+
+        if (!$task->hasExecutor($member->getId())) {
+            $task->assignExecutor($member);
+        }
+
+        $this->flusher->flush();
+    }
+}
+
