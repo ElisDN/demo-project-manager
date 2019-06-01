f9d6cc357e6f7ec979d028d16cd223f0a265c3f8
ElisDN <mail@elisdn.ru>
2019-06-01 16:47:10 +0300

    Added task edit usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Edit/Form.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\Edit;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', Type\TextType::class)
+            ->add('content', Type\TextareaType::class, ['required' => false, 'attr' => ['rows' => 10]]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Command::class,
+        ));
+    }
+}
+
