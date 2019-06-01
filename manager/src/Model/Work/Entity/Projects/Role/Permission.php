719df95a4b14352c93691cfe508213b86ab52e2b
ElisDN <mail@elisdn.ru>
2019-06-01 17:56:14 +0300

    Added tasks permissions

--- manager/src/Model/Work/Entity/Projects/Role/Permission.php
+++ manager/src/Model/Work/Entity/Projects/Role/Permission.php
@@ -9,6 +9,8 @@ use Webmozart\Assert\Assert;
 class Permission
 {
     public const MANAGE_PROJECT_MEMBERS = 'manage_project_members';
+    public const VIEW_TASKS = 'view_tasks';
+    public const MANAGE_TASKS = 'manage_tasks';
 
     private $name;
 
@@ -22,6 +24,8 @@ class Permission
     {
         return [
             self::MANAGE_PROJECT_MEMBERS,
+            self::VIEW_TASKS,
+            self::MANAGE_TASKS,
         ];
     }
 
--- manager/translations/work_permissions.en.php
+++ manager/translations/work_permissions.en.php
@@ -6,4 +6,6 @@ use App\Model\Work\Entity\Projects\Role\Permission;
 
 return [
     Permission::MANAGE_PROJECT_MEMBERS => 'Manage Project Members',
+    Permission::VIEW_TASKS => 'View Tasks',
+    Permission::MANAGE_TASKS => 'Manage tasks',
 ];
