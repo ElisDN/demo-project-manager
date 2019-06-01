c65343d0a0ce91a182f477a13d2f1eee685a7abc
ElisDN <mail@elisdn.ru>
2019-06-01 17:14:52 +0300

    Added task parent usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/ChildOf/Form.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\UseCase\Projects\Task\ChildOf;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('parent', Type\IntegerType::class, ['required' => false]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Command::class,
+        ));
+    }
+}
+
