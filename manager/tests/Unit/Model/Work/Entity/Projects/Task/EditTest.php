a53f9b648556e8730c22f390729bfd899931564c
ElisDN <mail@elisdn.ru>
2019-06-01 14:57:27 +0300

    Added task edit

---
+++ manager/tests/Unit/Model/Work/Entity/Projects/Task/EditTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\Work\Entity\Projects\Task;
+
+use App\Tests\Builder\Work\Members\GroupBuilder;
+use App\Tests\Builder\Work\Members\MemberBuilder;
+use App\Tests\Builder\Work\Projects\ProjectBuilder;
+use App\Tests\Builder\Work\Projects\TaskBuilder;
+use PHPUnit\Framework\TestCase;
+
+class EditTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $task->edit(
+            $name = 'New Name',
+            $content = 'New Content'
+        );
+
+        self::assertEquals($name, $task->getName());
+        self::assertEquals($content, $task->getContent());
+    }
+}
