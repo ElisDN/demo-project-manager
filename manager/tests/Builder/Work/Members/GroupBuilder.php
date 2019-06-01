ecb0e627913d75bb9ddfbd70744e91b1c1905efd
ElisDN <mail@elisdn.ru>
2019-06-01 14:50:57 +0300

    Added task create

---
+++ manager/tests/Builder/Work/Members/GroupBuilder.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Builder\Work\Members;
+
+use App\Model\Work\Entity\Members\Group\Group;
+use App\Model\Work\Entity\Members\Group\Id;
+
+class GroupBuilder
+{
+    private $name;
+
+    public function __construct()
+    {
+        $this->name = 'Group';
+    }
+
+    public function build(): Group
+    {
+        return new Group(
+            Id::next(),
+            $this->name
+        );
+    }
+}
