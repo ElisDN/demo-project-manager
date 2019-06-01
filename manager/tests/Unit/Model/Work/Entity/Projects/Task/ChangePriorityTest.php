57f26d7cc7fa9cf242f5cf23d0575e0863ef4348
ElisDN <mail@elisdn.ru>
2019-06-01 16:06:19 +0300

    Added priority changing

---
+++ manager/tests/Unit/Model/Work/Entity/Projects/Task/ChangePriorityTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\Work\Entity\Projects\Task;
+
+use App\Tests\Builder\Work\Members\GroupBuilder;
+use App\Tests\Builder\Work\Members\MemberBuilder;
+use App\Tests\Builder\Work\Projects\ProjectBuilder;
+use App\Tests\Builder\Work\Projects\TaskBuilder;
+use PHPUnit\Framework\TestCase;
+
+class ChangePriorityTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $task->changePriority($priority = 3);
+
+        self::assertEquals($priority, $task->getPriority());
+    }
+
+    public function testAlready(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $task->changePriority($priority = 3);
+
+        $this->expectExceptionMessage('Priority is already same.');
+        $task->changePriority($priority);
+    }
+
+    public function testIncorrect(): void
+    {
+        $group = (new GroupBuilder())->build();
+        $member = (new MemberBuilder())->build($group);
+        $project = (new ProjectBuilder())->build();
+        $task = (new TaskBuilder())->build($project, $member);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $task->changePriority(6);
+    }
+}
