701c3d5e4cbc926a507d5420ae1071a1f8e2406a
ElisDN <mail@elisdn.ru>
2019-06-08 19:05:11 +0300

    Installed Flysystem

--- manager/config/bundles.php
+++ manager/config/bundles.php
@@ -19,4 +19,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
 ];
