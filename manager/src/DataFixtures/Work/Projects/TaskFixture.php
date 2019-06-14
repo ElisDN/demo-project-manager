bee0bb3791ab20bb9e0ede3b3c5bbbed727a7bd9
ElisDN <mail@elisdn.ru>
2019-06-14 21:40:41 +0300

    Changes fixture task dates

--- manager/src/DataFixtures/Work/Projects/TaskFixture.php
+++ manager/src/DataFixtures/Work/Projects/TaskFixture.php
@@ -29,7 +29,7 @@ class TaskFixture extends Fixture implements DependentFixtureInterface
 
         $previous = [];
 
-        $date = new \DateTimeImmutable('-300 days');
+        $date = new \DateTimeImmutable('-100 days');
 
         for ($i = 0; $i < 100; $i++) {
 
