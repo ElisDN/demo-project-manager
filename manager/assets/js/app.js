0780029309586ccbe34ef091a811b92b46baacfa
ElisDN <mail@elisdn.ru>
2019-06-19 19:13:01 +0300

    Added Centrifuge auth tokens

--- manager/assets/js/app.js
+++ manager/assets/js/app.js
@@ -8,8 +8,11 @@ const toastr = require('toastr');
 
 document.addEventListener('DOMContentLoaded', function () {
     let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
+    let user = document.querySelector('meta[name=centrifugo-user]').getAttribute('content');
+    let token = document.querySelector('meta[name=centrifugo-token]').getAttribute('content');
     let centrifuge = new Centrifuge(url);
-    centrifuge.subscribe('alerts', function (message) {
+    centrifuge.setToken(token);
+    centrifuge.subscribe('alerts#' + user, function (message) {
         toastr.info(message.data.message);
     });
     centrifuge.connect();
