eb50e7196f234a74a77ef7bd2a8d1786399c39f4
ElisDN <mail@elisdn.ru>
2019-06-19 18:51:20 +0300

    Added toastr library

--- manager/assets/js/app.js
+++ manager/assets/js/app.js
@@ -4,12 +4,13 @@ require('bootstrap');
 require('@coreui/coreui');
 
 const Centrifuge = require('centrifuge');
+const toastr = require('toastr');
 
 document.addEventListener('DOMContentLoaded', function () {
     let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
     let centrifuge = new Centrifuge(url);
     centrifuge.subscribe('alerts', function (message) {
-        console.log(message.data.message);
+        toastr.info(message.data.message);
     });
     centrifuge.connect();
 });
