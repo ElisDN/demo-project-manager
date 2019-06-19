981252b649da6868fe14e783f58e3aa8edddc512
ElisDN <mail@elisdn.ru>
2019-06-19 18:18:42 +0300

    Added Centrifugo JS library

--- manager/assets/js/app.js
+++ manager/assets/js/app.js
@@ -2,3 +2,13 @@ require('../css/app.scss');
 
 require('bootstrap');
 require('@coreui/coreui');
+
+const Centrifuge = require('centrifuge');
+
+document.addEventListener('DOMContentLoaded', function () {
+    const centrifuge = new Centrifuge('ws://localhost:8083/connection/websocket');
+    centrifuge.subscribe('alerts', function (message) {
+        console.log(message.data.message);
+    });
+    centrifuge.connect();
+});
