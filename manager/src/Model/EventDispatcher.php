42937a58e6f018d46f96964d167d522635280055
ElisDN <mail@elisdn.ru>
2019-06-22 18:03:49 +0300

    Migrated events to messenger

--- manager/src/Model/EventDispatcher.php
+++ manager/src/Model/EventDispatcher.php
@@ -4,21 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model;
 
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-
-class EventDispatcher
+interface EventDispatcher
 {
-    private $dispatcher;
-
-    public function __construct(EventDispatcherInterface $dispatcher)
-    {
-        $this->dispatcher = $dispatcher;
-    }
-
-    public function dispatch(array $events): void
-    {
-        foreach ($events as $event) {
-            $this->dispatcher->dispatch($event);
-        }
-    }
+    public function dispatch(array $events): void;
 }
