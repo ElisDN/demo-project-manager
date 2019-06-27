b6479fee5e5b4e3dd65eed2f460b0c36019b0504
ElisDN <mail@elisdn.ru>
2019-06-27 19:35:39 +0300

    Added API home test

---
+++ manager/tests/Functional/Api/HomeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Api;
+
+use App\Tests\Functional\DbWebTestCase;
+
+class HomeTest extends DbWebTestCase
+{
+    public function testGet(): void
+    {
+        $this->client->request('GET', '/api');
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        self::assertJson($content = $this->client->getResponse()->getContent());
+
+        $data = json_decode($content, true);
+
+        self::assertEquals([
+            'name' => 'JSON API',
+        ], $data);
+    }
+
+    public function testPost(): void
+    {
+        $this->client->request('POST', '/api');
+
+        self::assertEquals(405, $this->client->getResponse()->getStatusCode());
+    }
+}
