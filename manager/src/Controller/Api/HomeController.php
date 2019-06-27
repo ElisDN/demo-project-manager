c60fbbfd17274295533c0fe4611d4a7b7b7c694c
ElisDN <mail@elisdn.ru>
2019-06-27 18:43:59 +0300

    Added API home controller

---
+++ manager/src/Controller/Api/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/api", name="api", methods={"GET"})
+     * @return Response
+     */
+    public function home(): Response
+    {
+        return $this->json([
+            'name' => 'JSON API',
+        ]);
+    }
+}
