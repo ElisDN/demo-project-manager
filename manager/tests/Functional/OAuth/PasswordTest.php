f7d974b545ec679a1cb531c04127d760c1edb758
ElisDN <mail@elisdn.ru>
2019-06-29 15:50:32 +0300

    Added oauth password grant test

---
+++ manager/tests/Functional/OAuth/PasswordTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\OAuth;
+
+use App\Tests\Functional\DbWebTestCase;
+
+class PasswordTest extends DbWebTestCase
+{
+    private const URI = '/token';
+
+    public function testMethod(): void
+    {
+        $this->client->request('GET', self::URI);
+        self::assertEquals(400, $this->client->getResponse()->getStatusCode());
+        self::assertJson($content = $this->client->getResponse()->getContent());
+
+        $data = json_decode($content, true);
+
+        self::assertArraySubset([
+            'error' => 'unsupported_grant_type',
+        ], $data);
+    }
+
+    public function testSuccess(): void
+    {
+        $this->client->request('POST', self::URI, [
+            'grant_type' => 'password',
+            'username' => 'oauth-password-user@app.test',
+            'password' => 'password',
+            'client_id' => 'oauth',
+            'client_secret' => 'secret',
+            'access_type' => 'offline',
+        ]);
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        self::assertJson($content = $this->client->getResponse()->getContent());
+
+        $data = json_decode($content, true);
+
+        self::assertArraySubset([
+            'token_type' => 'Bearer',
+        ], $data);
+
+        self::assertArrayHasKey('expires_in', $data);
+        self::assertNotEmpty($data['expires_in']);
+
+        self::assertArrayHasKey('access_token', $data);
+        self::assertNotEmpty($data['access_token']);
+
+        self::assertArrayHasKey('refresh_token', $data);
+        self::assertNotEmpty($data['refresh_token']);
+    }
+
+    public function testInvalid(): void
+    {
+        $this->client->request('POST', self::URI, [
+            'grant_type' => 'password',
+            'username' => 'oauth-password-user@app.test',
+            'password' => 'invalid',
+            'client_id' => 'oauth',
+            'client_secret' => 'secret',
+        ]);
+
+        self::assertEquals(401, $this->client->getResponse()->getStatusCode());
+    }
+}
