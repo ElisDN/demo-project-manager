5376cc2973696091da05a4087b16133bced7b8e3
ElisDN <mail@elisdn.ru>
2019-06-30 15:05:52 +0300

    Added projects api test

--- manager/tests/Builder/Work/Members/MemberBuilder.php
+++ manager/tests/Builder/Work/Members/MemberBuilder.php
@@ -12,19 +12,35 @@ use App\Model\Work\Entity\Members\Member\Name;
 
 class MemberBuilder
 {
+    private $id;
     private $name;
     private $email;
 
     public function __construct()
     {
+        $this->id = Id::next();
         $this->name = new Name('First', 'Last');
         $this->email = new Email('member@app.test');
+    }
+
+    public function withId(Id $id): self
+    {
+        $clone = clone $this;
+        $clone->id = $id;
+        return $clone;
+    }
+
+    public function withEmail(Email $email): self
+    {
+        $clone = clone $this;
+        $clone->email = $email;
+        return $clone;
     }
 
     public function build(Group $group): Member
     {
         return new Member(
-            Id::next(),
+            $this->id,
             $group,
             $this->name,
             $this->email
