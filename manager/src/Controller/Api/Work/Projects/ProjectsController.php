b1993dc38295234dd6cdfa597d19e14db76e0515
ElisDN <mail@elisdn.ru>
2019-06-30 15:07:44 +0300

    Added api projects controller

---
+++ manager/src/Controller/Api/Work/Projects/ProjectsController.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api\Work\Projects;
+
+use App\ReadModel\Work\Projects\Project\Filter;
+use App\ReadModel\Work\Projects\Project\ProjectFetcher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class ProjectsController extends AbstractController
+{
+    private const PER_PAGE = 50;
+
+    private $denormalizer;
+
+    public function __construct(DenormalizerInterface $denormalizer)
+    {
+        $this->denormalizer = $denormalizer;
+    }
+
+    /**
+     * @Route("/work/projects", name="work.projects", methods={"GET"})
+     * @param Request $request
+     * @param ProjectFetcher $fetcher
+     * @return Response
+     * @throws ExceptionInterface
+     */
+    public function index(Request $request, ProjectFetcher $fetcher): Response
+    {
+        if ($this->isGranted('ROLE_WORK_MANAGE_PROJECTS')) {
+            $filter = Filter\Filter::all();
+        } else {
+            $filter = Filter\Filter::forMember($this->getUser()->getId());
+        }
+
+        /** @var Filter\Filter $filter */
+        $filter = $this->denormalizer->denormalize($request->query->get('filter', []), Filter\Filter::class, 'array', [
+            'object_to_populate' => $filter,
+            'ignored_attributes' => ['member'],
+        ]);
+
+        $pagination = $fetcher->all(
+            $filter,
+            $request->query->getInt('page', 1),
+            self::PER_PAGE,
+            $request->query->get('sort', 'sort'),
+            $request->query->get('direction', 'asc')
+        );
+
+        return $this->json([
+            'items' => array_map(static function (array $item) {
+                return [
+                    'id' => $item['id'],
+                    'name' => $item['name'],
+                    'status' => $item['status'],
+                ];
+            }, (array)$pagination->getItems()),
+            'pagination' => [
+                'count' => $pagination->count(),
+                'total' => $pagination->getTotalItemCount(),
+                'per_page' => $pagination->getItemNumberPerPage(),
+                'page' => $pagination->getCurrentPageNumber(),
+                'pages' => ceil($pagination->getTotalItemCount() / $pagination->getItemNumberPerPage()),
+            ]
+        ]);
+    }
+}
