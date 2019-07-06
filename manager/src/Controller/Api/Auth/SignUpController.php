d8207c71060fad2f512ea5d5fd43e37d021a2253
ElisDN <mail@elisdn.ru>
2019-07-06 19:22:17 +0300

    Added action annotations

--- manager/src/Controller/Api/Auth/SignUpController.php
+++ manager/src/Controller/Api/Auth/SignUpController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Api\Auth;
 
 use App\Model\User\UseCase\SignUp;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,6 +26,40 @@ class SignUpController extends AbstractController
     }
 
     /**
+     * @OA\Post(
+     *     path="/auth/signup",
+     *     tags={"Sign Up"},
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             type="object",
+     *             required={"first_name", "last_name", "email", "password"},
+     *             @OA\Property(property="first_name", type="string"),
+     *             @OA\Property(property="last_name", type="string"),
+     *             @OA\Property(property="email", type="string"),
+     *             @OA\Property(property="password", type="string"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Success response",
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Errors",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="object", nullable=true,
+     *                 @OA\Property(property="code", type="integer"),
+     *                 @OA\Property(property="message", type="string"),
+     *             ),
+     *             @OA\Property(property="violations", type="array", nullable=true, @OA\Items(
+     *                 type="object",
+     *                 @OA\Property(property="propertyPath", type="string"),
+     *                 @OA\Property(property="title", type="string"),
+     *             ))
+     *         )
+     *     ),
+     * )
      * @Route("/auth/signup", name="auth.signup", methods={"POST"})
      * @param Request $request
      * @param SignUp\Request\Handler $handler
--- manager/src/Controller/Api/Profile/ShowController.php
+++ manager/src/Controller/Api/Profile/ShowController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Api\Profile;
 
 use App\Model\User\Entity\User\Network;
 use App\ReadModel\User\UserFetcher;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ShowController extends AbstractController
 {
     /**
+     * @OA\Get(
+     *     path="/profile",
+     *     tags={"Profile"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success response",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer"),
+     *             @OA\Property(property="email", type="string"),
+     *             @OA\Property(property="name", type="object",
+     *                 @OA\Property(property="first", type="string"),
+     *                 @OA\Property(property="last", type="string"),
+     *             ),
+     *             @OA\Property(property="networks", type="array", @OA\Items(
+     *                 type="object",
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="identity", type="string"),
+     *             ))
+     *         )
+     *     ),
+     *     security={{"oauth2": {"common"}}}
+     * )
      * @Route("/profile", name="profile", methods={"GET"})
      * @param UserFetcher $users
      * @return Response
