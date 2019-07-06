6fdecaae51787d37c5d3a84f6a8ff47617ea9c03
ElisDN <mail@elisdn.ru>
2019-07-06 18:56:42 +0300

    Added OA annotations

--- manager/src/Controller/Api/HomeController.php
+++ manager/src/Controller/Api/HomeController.php
@@ -4,13 +4,48 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Manager API",
+ *     description="HTTP JSON API",
+ * ),
+ * @OA\Server(
+ *     url="/api"
+ * ),
+ * @OA\SecurityScheme(
+ *     type="oauth2",
+ *     securityScheme="oauth2",
+ *     @OA\Flow(
+ *         flow="implicit",
+ *         authorizationUrl="/authorize",
+ *         scopes={
+ *             "common": "Common"
+ *         }
+ *     )
+ * )
+ */
 class HomeController extends AbstractController
 {
     /**
+     * @OA\Get(
+     *     path="/",
+     *     tags={"API"},
+     *     description="API Home",
+     *     @OA\Response(
+     *         response="200",
+     *         description="Success response",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="name", type="string")
+     *         )
+     *     )
+     * )
      * @Route("", name="home", methods={"GET"})
      * @return Response
      */
