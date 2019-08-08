0002a3201191e93f90dbd066d3f2edd3edd4513a
ElisDN <mail@elisdn.ru>
2019-08-08 15:55:48 +0300

    Extracted annotations Error Model

--- manager/src/Controller/Api/Profile/NameController.php
+++ manager/src/Controller/Api/Profile/NameController.php
@@ -44,18 +44,7 @@ class NameController extends AbstractController
      *     @OA\Response(
      *         response=400,
      *         description="Errors",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="error", type="object", nullable=true,
-     *                 @OA\Property(property="code", type="integer"),
-     *                 @OA\Property(property="message", type="string"),
-     *             ),
-     *             @OA\Property(property="violations", type="array", nullable=true, @OA\Items(
-     *                 type="object",
-     *                 @OA\Property(property="propertyPath", type="string"),
-     *                 @OA\Property(property="title", type="string"),
-     *             ))
-     *         )
+     *         @OA\JsonContent(ref="#/components/schemas/ErrorModel")
      *     ),
      *     security={{"oauth2": {"common"}}}
      * )
